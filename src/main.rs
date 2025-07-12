058a0269d26ef0b88134b7ea457d7563e6c42bb3
Kenneth Napura <senpai24kenjie@gmail.com>
2025-07-12 23:56:44 +0800

    Memory borrow and ownership transfer

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,33 @@
 use std::ffi::c_void;
 
-fn main() {
+fn main()
+{
+    memory_demo();
     for x in 1..5
     {
         setProfile(x, "Ken", 23, 'M', 400.00f32, false);
     }
 }
 
+fn memory_demo()
+{
+    // Transfer / Moving References or Values
+    let a: String = String::from("A Memory");
+    let b: String = a;
+
+    // println!("{a}"); Can't call this because a has no value
+    println!("{b}");
+
+    // Borrowing
+    let c: &String = &b;
+    println!("{c}");
+
+}
 
 fn setProfile(index: i8, name: &str, age: i8, gender: char, balance: f32, is_married: bool)
 {
+    let index_text: String = index.to_string();
+    let id: &[u8] = index_text.as_bytes();
     let _gender: &str = if gender == 'M' { "Male" } else { "Female"};
     println!("[{index}] ----------------------------- ");
     println!("User: {name}");
@@ -17,4 +35,12 @@ fn setProfile(index: i8, name: &str, age: i8, gender: char, balance: f32, is_mar
     println!("Gender: {_gender}");
     println!("Balance: {balance}");
     println!("Is Married: {is_married}");
+    let mut x: String = String::from("");
+
+    for byte in id.iter()
+    {
+        x.push_str(byte.to_string().as_str());
+    };
+
+    println!("Name bytes: {x}");
 }
